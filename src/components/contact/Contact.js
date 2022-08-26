e75f4a563c9e2e1294d7542bd3bfe08ef9f7db02
D_Zucho <danny.nico.dev@outlook.com>
2022-08-26 03:54:43 -0700

    start contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,62 @@
 import './contact.style.css'
-
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
 const Contact = () => {
   return (
     <section id='contact'>
-      <p>contact</p>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <div className='email__container'>
+              <MdOutlineEmail />
+              <h4>email</h4>
+            </div>
+            <h5>dummyemail@gmail.com</h5>
+            <a href='mailto:danny.nicodev@gmail.com'>Send a Messege</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>danny_nicoDev</h5>
+            <a href='https://facebook.com' target='_blank' rel='noreferrer'>
+              Send a Message
+            </a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp />
+            <h4>email</h4>
+            <h5>+1-234-5678</h5>
+            <a
+              href='https://api.whatsapp.com/send?phone=19494687544'
+              target='_blank'
+              rel='noreferrer'>
+              Send a Message
+            </a>
+          </article>
+        </div>
+        <div className='contact__form'>
+          <form action=''>
+            <input
+              type='text'
+              name='name'
+              placeholder='Your Full Name'
+              required
+            />
+            <input
+              type='email'
+              name='email'
+              placeholder='Your Email'
+              required
+            />
+            <textarea name='message' rows='7' required></textarea>
+            <button className='btn btn-primary'>Send a Message!</button>
+          </form>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,10 +1,69 @@
 import './testimonials.style.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import { Navigation, Pagination } from 'swiper'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, similique atque assumenda aliquam unde repudiandae blanditiis itaque dolore autem deserunt.',
+  },
+  {
+    avatar: AVTR2,
+    name: 'Shatta Wale',
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, similique atque assumenda aliquam unde repudiandae blanditiis itaque dolore autem deserunt.',
+  },
+  {
+    avatar: AVTR3,
+    name: 'Kwame Despite',
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, similique atque assumenda aliquam unde repudiandae blanditiis itaque dolore autem deserunt.',
+  },
+  {
+    avatar: AVTR4,
+    name: 'Lesley Ormani',
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, similique atque assumenda aliquam unde repudiandae blanditiis itaque dolore autem deserunt.',
+  },
+]
 
 const Testimonials = () => {
   return (
-    <div>
-      <h1>testimonials</h1>
-    </div>
+    <section>
+      <h5>Review from Client</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        className='container testimonials__container'>
+        {data.map(({ avatar, name, review }) => (
+          <SwiperSlide key={name} className='testimonial'>
+            <div className='client__avatar'>
+              <img src={avatar} alt={name} />
+            </div>
+            <h5 className='client__name'>{name}</h5>
+            <small className='client__review'>{review}</small>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   )
 }
 
